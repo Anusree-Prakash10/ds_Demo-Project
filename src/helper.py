f9cc226f2b418da7fb269657935db76cd39bd08a
Anusree-Prakash10 <prakashukanusree@gmail.com>
2026-05-15 12:21:32 +0530

    added src

---
+++ src/helper.py
@@ -0,0 +1,12 @@
+import yaml
+from os.path import join
+from pathlib import Path
+
+CONFIG_PATH = Path(__file__).parent / "config"
+config_name = "config.yaml"
+
+
+def load_config(config_name=config_name):
+    with open(join(CONFIG_PATH, config_name), "r") as f:
+        config = yaml.safe_load(f)
+    return config
